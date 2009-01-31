22fbb65ff6c2f7386579e95a26506d42eae0a21e
Indemnity83 <Indemnity83@ef5e3370-edc8-11dd-b787-91885b911a96>
2009-01-31 13:49:18 +0000

    Finalized shortcode for a single author. Example [author]admin[/author] or [author id='1']. Also added copy of GNU license.

--- author-digest.php
+++ author-digest.php
@@ -4,6 +4,20 @@
  * @author Kyle Klaus
  * @version 1.0.0
  * @abstract Tools for multi-author WordPress installations.
+ * @copyright Copyright 2009  Kyle Klaus  (email : kklaus@indemnity83.com)
+ * @license This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 /*
@@ -15,23 +29,6 @@ Version: 1.0.0
 Author URI: http://indemnity83.com
 */
 
-/*  Copyright 2009  Kyle Klaus  (email : kklaus@indemnity83.com)
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-
 /**
  * The current plugin version
  */
@@ -40,7 +37,7 @@ define('ADIGEST_VERSION','1.0.0');
 add_action('wp_head', 'adigest_css');
 add_action('admin_menu', 'adigest_menu');
 
-add_shortcode('author','adigest_shortcode');
+add_shortcode('author','adigest_author_shortcode');
 
 register_activation_hook(__FILE__,'adigest_activate');
 register_deactivation_hook( __FILE__, 'adigest_deactivate' );
@@ -51,6 +48,7 @@ register_deactivation_hook( __FILE__, 'adigest_deactivate' );
 function adigest_activate() {
 	add_option('adigest_version', ADIGEST_VERSION);
 }
+
 
 /**
  * Perform some cleanup, and deactivate the plugin
@@ -87,48 +85,55 @@ function adigest_options() {
 }
 
 /**
- * Handle the shortcode in content
+ * Handle the 'author' shortcode in content
  * @var array shortcode attributes
  * @var string shortcode content
  * @return string formatted shortcode replacement
  */
-function adigest_shortcode($atts,$content = null) {
+function adigest_author_shortcode($atts,$content = null) {
 	extract( shortcode_atts( array(
-    	'expanded' => true,
+    	'mode'	=> 'expanded',
+		'id'	=> ''
 	), $atts ) );
 	
-    if( $content != null ) {
-		if( $expanded == true ) {
-			$authid = $content;
-      		return adigest_author(get_userdata($authid));
-      	}
+	if( $id == '' ) {
+		$author = get_userdatabylogin($content);
+	} else {
+		$author = get_userdata(intval($id));
 	}
 	
-	return 'Author Digest Error';
+	switch( $mode ) {
+		case 'expanded':
+      		return adigest_author($author);
+      		break;
+		case 'list':
+			return adigest_list();
+      		break;
+		default:
+			return 'Invalid Mode';
+			break;
+	}
 }
 
-
-
 /**
- * Return the expanded author content
- * @var object author meta
- * @return string expanded author content
+ * Output a single author definition
  */
 function adigest_author($author) {
-	$content  = '<dl>';
-	$content .= '	<dt><a href="http://www.prudentialnorcal.com/?author='.$author->ID.'" />'.$author->first_name.' '.$author->last_name.'</a></dt>';
-	$content .= '	<dd class="img"><a href="http://www.prudentialnorcal.com/?author='.$author->ID.'" /><img src="http://www.prudentialnorcal.com/wp-content/profile-pics/'.$author->ID.'.jpg" width="80" /></a></dd>';
-	$content .= '	<dd></dd>';
-	$content .= '</dl>';
-
-	return $content;
+	$body  = '<div class="author">';
+	$body .= '  <dl>';
+	$body .= '	  <dt><a href="http://www.prudentialnorcal.com/?author='.$author->ID.'" />'.$author->nickname.'</a></dt>';
+	$body .= '	  <dd class="img"><a href="http://www.prudentialnorcal.com/?author='.$author->ID.'" />'.get_avatar( $author->ID, $size = '80' ).'</a></dd>';
+	$body .= '	  <dd>Welcome to WordPress. This is your first post. Edit or delete it, then start blogging!</dd>';
+	$body .= '  </dl>';
+	$body .= '</div>';
+	return $body;
 }
 
 /**
  * Output the Author List
  */
 function adigest_list($options) {
-	
+	$author = get_userdata(intval($id));
 }
 
 /**
@@ -137,7 +142,45 @@ function adigest_list($options) {
 function adigest_css() {
 	echo "
 	<style type='text/css'>
-
+		.author {
+			float: left;
+			width: 500px;
+			padding: 0;	
+		}
+		
+		.author dl {
+			float: left;
+			width: 460px;
+			margin: 10px 20px;
+			padding: 0;
+			display: inline; /* fixes IE/Win double margin bug */
+		}
+		
+		.author dt {
+			float: right;
+			width: 362px;
+			margin: 0;
+			padding: 0;
+			font-size: 130%;
+			letter-spacing: 1px;
+		}
+		
+		.author dd {
+			margin: 0;
+			padding: 0;
+			font-size: 85%;
+			line-height: 1.5em;	
+		}
+		
+		.author dd.img img {
+			float: left;
+			margin: 0 8px 0 0;
+			padding: 4px;
+			border: 1px solid #D9E0E6;
+			border-bottom-color: #C8CDD2;
+			border-rright-color: #C8CDD2;
+			background: #FFF;	
+		}	
 	</style>
 	";
 }
